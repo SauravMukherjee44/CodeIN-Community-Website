3935b07aadc2791508d27dc669e81e94caf65e82
Pallavi679 <pallavi.pandey_19@sakec.ac.in>
2021-10-03 17:37:50 +0530

    Responsive

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if(window.scrollY>60){
+            document.querySelector('#scroll-top').classList.add('active');
+        }else{
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+
+        // scroll spy
+        $('section').each(function(){
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if(top>offset && top<offset+height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },500, 'linear')
+    })
+});
