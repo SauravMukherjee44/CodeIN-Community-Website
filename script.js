b20c8925b4fbaff4259f04d1d3b0d8ba4fbdd378
saurav <mesouravofficial@gmail.com>
2021-10-08 02:55:12 +0530

    Twak.to Added

--- script.js
+++ script.js
@@ -37,3 +37,16 @@ $(document).ready(function(){
         },500, 'linear')
     })
 });
+
+//Twak.to Added
+//Live chat Widget
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/615f64df157d100a41ab54f3/1fhea43dh';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
