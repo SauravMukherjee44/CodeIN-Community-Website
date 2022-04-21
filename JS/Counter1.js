dd133836958336db0bcef4f0fbfebfd6da234a3e
MUHAMMAD SAMIULLAH <samipak458@gmail.com>
2022-04-21 22:48:26 +0500

    Subscribe Newsletter added

--- JS/Counter1.js
+++ JS/Counter1.js
@@ -4,3 +4,18 @@ $(document).ready(function(){
       time: 400
     });
   });
+
+  // Subscribe Newsletter
+  let button = document.getElementById("btns");
+  
+button.addEventListener('click', (e)=> {
+  e.preventDefault();
+  
+  let msg = document.getElementById("suscribeMsg");
+  let email = document.getElementById("Email");
+    
+  email.value = "";
+  msg.innerText = "Sucessfully! Subscribed";
+  msg.style.color = "#f195a3";
+    
+})
