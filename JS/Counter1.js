4adca0a05ebd473e70cb431ca0b8c9e97fab5b88
MUHAMMAD SAMIULLAH <samipak458@gmail.com>
2022-04-21 15:05:51 +0500

    Update Counter1.js

--- JS/Counter1.js
+++ JS/Counter1.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
       time: 400
     });
   });
+
+// Modal JS
+let button = document.getElementById("btns");
+  
+button.addEventListener('click', (e)=> {
+  e.preventDefault();
+  
+  let msg = document.getElementById("suscribeMsg");
+  let email = document.getElementById("Email");
+    
+  email.value = "";
+  msg.innerText = "Sucessfully! Subscribed";
+  msg.style.color = "#f195a3";
+    
+})
+  
