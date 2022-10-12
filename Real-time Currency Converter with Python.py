77c4dab9300270176954a6dfe8b479420acba1c6
MR. ROBOT <114983115+mr-robot-official@users.noreply.github.com>
2022-10-12 21:36:58 +0530

    Real-time Currency Converter with Python.py

---
+++ Real-time Currency Converter with Python.py
@@ -0,0 +1,8 @@
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+amount = int(input("Enter the amount: "))
+from_currency = input("From Currency: ").upper()
+to_currency = input("To Currency: ").upper()
+print(from_currency, " To ", to_currency, amount)
+result = c.convert(from_currency, to_currency, amount)
+print(result)
